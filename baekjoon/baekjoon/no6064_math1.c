b5c79edd4a5e56b5f86ea47722301d55b7410353
ys <lys984prms@naver.com>
2019-07-15 22:21:01 +0900

    6064 정답

--- baekjoon/baekjoon/no6064_math1.c
+++ baekjoon/baekjoon/no6064_math1.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
-
+/*
+15
+40000 39999 39999 39998
+40000 39999 40000 39999
+40000 40000 40000 39999
+40000 39998 40000 39997
+39999 2 39998 2
+3 40000 3 39999
+40000 3 40000 3
+8 2 4 2
+10 12 2 12
+12 10 12 10
+12 10 1 1
+12 6 12 6
+10 1 5 1
+1 10 1 5
+1 1 1 1
+*/
 int main() {
 	int T, result[10000] = { 0, };
 	scanf("%d", &T);
@@ -11,14 +28,22 @@ int main() {
 		int last = gcd(m,n);
 		result[i] = -1;
 		int k = x;
-		while (k < last) {
-			if (k%n == y) {
+		if (k == 0) {
+			k = m;
+		}
+		while (k <= last) {
+			int kn = k % n;
+			if (kn == 0) {
+				kn = n;
+			}
+			//printf("k : %d kn:%d\n", k, kn);
+			if (kn == y) {
 				result[i] = k;
 				break;
 			}
 			k += m;
 		}
-
+		//printf("result : %d\n\n", result[i]);
 	}
 	for (int i = 0; i < T; i++) {
 		printf("%d", result[i]);
@@ -35,5 +60,6 @@ int gcd(int a, int b) {
 		A = B;
 		B = temp;
 	}
+	//printf("최소공배수 : %d\n", a * b / A);
 	return a * b / A;
 }
