c35dbf17f6ed5a288b9d392d282dceabef0d6055
ys <lys984prms@naver.com>
2019-09-02 23:23:13 +0900

    1149 정답

--- baekjoon/baekjoon/no1149_dynamic.c
+++ baekjoon/baekjoon/no1149_dynamic.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
-char line[1000];
 int n;
 struct home* h;
-int total = 3001;
-
-int isNext(int depth, char color, int pre);
-int min(int a, int b, int c);
+int min(int a, int b) { if (a < b) return a; else return b; }
 
 struct home {
 	int r, g, b;
+	int sumR, sumG, sumB;
 };
 
 int main() {
@@ -19,63 +16,34 @@ int main() {
 		scanf("%d%d%d", &h[i].r, &h[i].g, &h[i].b);
 	}
 
-	isNext(0, 'a',0);
-	printf("%d", total);
+	h[0].sumB = h[0].b;
+	h[0].sumG = h[0].g;
+	h[0].sumR = h[0].r;
 
-	return 0;
-}
-int isNext(int depth, char color, int pre) {
-	if (depth == n - 1) {
-		if (total > pre + min(h[depth].r, h[depth].g, h[depth].b)) {
-			int r = h[depth].r;
-			int g = h[depth].g;
-			int b = h[depth].b;
-			if (color == 'r')	r = 1001;
-			else if (color == 'g')	g = 1001;
-			else if (color == 'b')	b = 1001;
-			total = pre + min(r,g,b);
-		}
-		return 0;
-	}
-
-	if (pre > total) {
-		return 0;
+	for (int i = 1; i < n; i++) {
+		h[i].sumB = h[i].b + min(h[i - 1].sumG, h[i - 1].sumR);
+		h[i].sumG = h[i].g + min(h[i - 1].sumR, h[i - 1].sumB);
+		h[i].sumR = h[i].r + min(h[i - 1].sumB, h[i - 1].sumG);
 	}
 
 
-	if (color != 'r'){
-		isNext(depth + 1, 'r', pre + h[depth].r);
-	}
-	if (color != 'g') {
-		isNext(depth + 1, 'g', pre + h[depth].g);
-	}
-	if (color != 'b') {
-		isNext(depth + 1, 'b', pre + h[depth].b);
-	}
-
-	return 0;
-}
-/*
-3
-26 40 83
-49 60 57
-13 89 99
-*/
-int min(int a, int b, int c) {
-	if (a < b) {
-		if (a < c) {
-			return a;
+	if (h[n - 1].sumB < h[n - 1].sumG) {
+		if (h[n - 1].sumB < h[n - 1].sumR) {
+			printf("%d", h[n - 1].sumB);
 		}
 		else {
-			return c;
+			printf("%d", h[n - 1].sumR);
 		}
 	}
 	else {
-		if (b < c) {
-			return b;
+		if (h[n - 1].sumG < h[n - 1].sumR) {
+			printf("%d", h[n - 1].sumG);
 		}
 		else {
-			return c;
+			printf("%d", h[n - 1].sumR);
 		}
 	}
+
+
+	return 0;
 }
