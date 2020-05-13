9e83266d91abe4df91d2db31310d6f9e4d474edb
ys <lys984prms@naver.com>
2020-05-13 23:43:41 +0900

    10156 정답

---
+++ baekjoon/baekjoon/no10159.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main() {
+	int n,m;
+	int x, y;
+	scanf("%d%d", &n, &m);
+	bool **arr = new bool*[n];
+	int *result = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = new bool[n];
+		result[i] = n-1;
+	}
+	for (int i = 0; i < m; i++) {
+		scanf("%d%d", &x, &y);
+		if (arr[x - 1][y - 1] != true) {
+			arr[x - 1][y - 1] = true;
+			result[x - 1]--;
+			result[y - 1]--;\
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (x = 0; x < n; x++) {
+			for (y = 0; y < n; y++) {
+				if (!(x == i) && !(y == i) && !(x == y)) {
+					if (arr[x][i] == true && arr[i][y] == true && arr[x][y] != true) {
+						arr[x][y] = true;
+						result[x]--;
+						result[y]--;
+						printf("%d %d %d\n", x + 1, i + 1, y + 1);
+					}
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", result[i]);
+	}
+	return 0;
+}
--- baekjoon/baekjoon/no1753_dijkstra.cpp
+++ baekjoon/baekjoon/no1753_dijkstra.cpp
@@ -1,60 +1,74 @@
-#include<stdio.h>
+#include<iostream>
 #include<vector>
 #include<queue>
-#include<iostream>
 
 using namespace std;
+int node, e;
+int INF = 1000000000;
 
-vector<pair<int,int> > a[20000];
-int *d;
-int INF = 300000;
+vector<pair<int, int>> a[200001];
+int d[200001];
 
-void dijkstra(int start) {
+void dijkstra(int start)
+{
 	d[start] = 0;
-	priority_queue<pair<int, int> > pq;
-	pq.push(make_pair(start, 0));
-	while (!pq.empty()) {
-		int current = pq.top().first;
-		int distance = -pq.top().second;
-		pq.pop();
+	priority_queue<pair<int, int>> pq; //힙구조 큰 순서부터 맨 앞에 있음 
+	pq.push(make_pair(0, start));
 
-		if (d[current] < distance) continue;
-		for (int i = 0; i < a[current].size(); i++) {
+	while (!pq.empty())
+	{
+		int current = pq.top().second;
+		int distance = -pq.top().first;
+		pq.pop();
+		//v[current] = 1;
+
+		if (d[current] < distance) continue; //최단 거리가 아닌 경우 스킵 
+
+		for (int i = 0; i < a[current].size(); i++)
+		{
 			int next = a[current][i].first;
-			int nextDis = distance + a[current][i].second;
-			if (nextDis < d[next]) {
-				d[next] = nextDis;
-				pq.push(make_pair(next, -nextDis));
+			int nextDistance = distance + a[current][i].second;
+
+			if (d[next] > nextDistance)
+			{
+				d[next] = nextDistance;
+				pq.push(make_pair(-nextDistance, next));
 			}
 		}
+
+
 	}
 }
-int main() {
-	int v, e;
-	int startV;
-	scanf("%d%d%d", &v, &e, &startV);
-	a->resize(v);
-	d = new int[v+1];
 
-	for (int i = 1; i <= v; i++) {
+int main()
+{
+
+	cin >> node >> e;
+
+	for (int i = 1; i <= node; i++)
 		d[i] = INF;
+
+	int start;
+	cin >> start;
+
+	int u, v, w;
+
+	for (int i = 1; i <= e; i++)
+	{
+		scanf("%d %d %d", &u, &v, &w);
+		a[u].push_back(make_pair(v, w));
 	}
 
-	for (int i = 0; i < e; i++) {
-		int start, end, c;
-		scanf("%d%d%d", &start, &end, &c);
-		a[start].push_back(make_pair(end, c));
-	}
 
-	dijkstra(startV);
+	dijkstra(start);
 
-	for (int i = 1; i <= v; i++) {
-		if (d[i] == INF) {
-			printf("INF ");
-		}
-		else {
-			printf("%d ", d[i]);
-		}
+	for (int i = 1; i <= node; i++)
+	{
+		if (d[i] == INF)
+			printf("INF\n");
+		else
+			printf("%d\n", d[i]);
+
 	}
 
 	return 0;
