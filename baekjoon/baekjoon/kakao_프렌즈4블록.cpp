ca2c01c6bed0d74915528fb0b98c4dfed3a3e559
ys <lys984prms@naver.com>
2020-06-23 17:46:02 +0900

    kakao 프렌즈4블록 정답

--- baekjoon/baekjoon/kakao_프렌즈4블록.cpp
+++ baekjoon/baekjoon/kakao_프렌즈4블록.cpp
@@ -5,140 +5,153 @@
 #include <iostream>
 
 using namespace std;
+
 int block[30][30];
-//1:블록 있음 0:지울수 있는 블록 -1:블록없음
-
 int same[4][2] = { {0,0},{1,0},{0,1},{1,1} };
-
-int checkPang(int x, int y, vector<string> board) {
-	int result = 0;
-	if (block[x][y] == -1 || board[x][y] == 'x') return 0;
-
-	if (board[x][y] == board[x + 1][y]
-		&& board[x + 1][y] == board[x][y + 1]
-		&& board[x][y + 1] == board[x + 1][y + 1]) {
+bool checkPang(int x, int y, vector<vector<char> > board2) {
+	if (y + 1 > board2[x + 1].size() - 1) return false;
+	bool answer = false;
+	if (board2[x][y] == board2[x + 1][y]
+		&& board2[x + 1][y] == board2[x][y + 1]
+		&& board2[x][y + 1] == board2[x + 1][y + 1]) {
 		for (int k = 0; k < 4; k++) {
 			if (block[x + same[k][0]][y + same[k][1]] != 0) {
 				block[x + same[k][0]][y + same[k][1]] = 0;
-				result++;
-			}
-		}
-	}
-	return result;
-}
-int solution(int m, int n, vector<string> board) {
-	int answer = 0;
-	for (int i = 0; i < m; i++) {
-		fill(block[i], block[i] + n, 1);
-	}
-	queue<int> none;
-	while (true) {
-		int change = 0;
-		for (int i = 0; i < m - 1; i++) {
-			for (int j = 0; j < n - 1; j++) {
-				change += checkPang(i, j, board);
-			}
-		}
-		if (change == 0) break;
-		else answer += change;
-		int ex = -1;
-		for (int j = 0; j < n; j++) {
-			for (int i = m - 1; i >= 0; i--) {
-				if (block[i][j] == -1) {
-					ex = i;
-					break;
-				}
-				if (!none.empty() && block[i][j] == 1) ex = i;
-				if (block[i][j] == 0) none.push(i);
-				if (!none.empty() && ex != -1) {
-					int pre = none.front();
-					none.pop();
-					board[pre].replace(j, 1, board[ex], j, 1);
-					board[ex].replace(j, 1, "x");
-					block[pre][j] = 1;
-					block[ex][j] = 0;
-					none.push(ex);
-					ex = -1;
-				}
-			}
-			while (!none.empty()) {
-				block[none.front()][j] = -1;
-				none.pop();
-			}
-			while (ex >= 0) {
-				block[ex][j] = -1;
-				ex--;
+				board2[x + same[k][0]][y + same[k][1]] = 'x';
+				answer = true;
 			}
 		}
 	}
 	return answer;
 }
+int solution(int m, int n, vector<string> board) {
+	int answer = 0;
+	vector<vector<char> > board2;
+	board2.resize(n);
+	for (int i = m - 1; i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			board2[j].push_back(board[i][j]);
+		}
+	}
+	for (int i = 0; i < n; i++) fill(block[i], block[i] + m, 1);
+
+	bool change;
+	while (true) {
+		change = false;
+		for (int i = 0; i < board2.size(); i++) {
+			if (board2[i].size() == 1 && board2[i][0] != 'e') {
+				board2[i].push_back('t');
+				board2[i].erase(board2[i].begin(), board2[i].begin() + 1);
+			}
+			else {
+				if (i == board2.size() - 1) continue;
+				for (int j = 0; j < board2[i].size() - 1; j++) {
+					change = checkPang(i, j, board2) == true ? true : change;
+				}
+			}
+		}
+		if (change == false) break;
+		else change = false;
+
+		for (int i = 0; i < board2.size(); i++) {
+			for (int j = 0; j < board2[i].size(); j++) {
+				board2[i][j] = block[i][j] == 0 ? 'x' : board2[i][j];
+			}
+		}
+
+		for (int i = 0; i < board2.size(); i++) {
+			bool check = false;
+			vector<char>::iterator iter;
+			for (iter = board2[i].begin(); iter != board2[i].end(); ) {
+				if ((*iter) == 'x') {
+					if (iter + 1 == board2[i].end() && check == false) {
+						board2[i].push_back('e');
+						iter = board2[i].erase(iter);
+					}
+					else iter = board2[i].erase(iter);
+				}
+				else {
+					check = true;
+					++iter;
+				}
+			}
+			fill(block[i], block[i] + m, 1);
+		}
+
+	}
+
+	for (int i = 0; i < board2.size(); i++) {
+		if (board2[i][0] == 'e') continue;
+		answer += board2[i].size();
+	}
+	return m * n - answer;
+}
 int main() {
 	vector<string> v;
-	//v.push_back("TTTANT");
-	//v.push_back("RRFACC");
-	//v.push_back("RRRFCC");
-	//v.push_back("TRRRAA");
-	//v.push_back("TTMMMF");
-	//v.push_back("TMMTTJ");
-	//cout << solution(6, 6, v) << endl;//15
+	v.push_back("TTTANT");
+	v.push_back("RRFACC");
+	v.push_back("RRRFCC");
+	v.push_back("TRRRAA");
+	v.push_back("TTMMMF");
+	v.push_back("TMMTTJ");
+	cout << solution(6, 6, v) << endl;//15
 
-	//
-	//v.clear();
-	//v.push_back("CCBDE");
-	//v.push_back("AAADE");
-	//v.push_back("AAABF");
-	//v.push_back("CCBBF");
-	//cout << solution(4, 5, v) << endl;//14
-
-	//v.clear();
-	//v.push_back("AA");
-	//v.push_back("AA");
-	//cout << solution(2, 2, v) << endl;//4
-
-	//v.clear();
-	//v.push_back("AA");
-	//v.push_back("AA");
-	//v.push_back("AB");
-	//cout << solution(3, 2, v) << endl;//4
-
-	//v.clear();
-	//v.push_back("CC");
-	//v.push_back("AA");
-	//v.push_back("AA");
-	//v.push_back("CC");
-	//cout << solution(4, 2, v) << endl;//8
-
-	//v.clear();
-	//v.push_back("FF");
-	//v.push_back("AA");
-	//v.push_back("CC");
-	//v.push_back("AA");
-	//v.push_back("AA");
-	//v.push_back("CC");
-	//v.push_back("DD");
-	//v.push_back("FF");
-	//cout << solution(8, 2, v) << endl;//8
-
-	//v.clear();
-	//v.push_back("HGNHU");
-	//v.push_back("CRSHV");
-	//v.push_back("UKHVL");
-	//v.push_back("MJHQB");
-	//v.push_back("GSHOT");
-	//v.push_back("MQMJJ");
-	//v.push_back("AGJKK");
-	//v.push_back("QULKK");
-	//cout << solution(8, 5, v) << endl;//8
+	
+	v.clear();
+	v.push_back("CCBDE");
+	v.push_back("AAADE");
+	v.push_back("AAABF");
+	v.push_back("CCBBF");
+	cout << solution(4, 5, v) << endl;//14
 
 	v.clear();
-	v.push_back("KKNKK");
-	v.push_back("KKSKK");
-	v.push_back("UKHVL");
-	v.push_back("KKMJJ");
-	v.push_back("KKJKK");
-	v.push_back("KKLKK");
+	v.push_back("aa");
+	v.push_back("aa");
+	cout << solution(2, 2, v) << endl;//4
+
+	v.clear();
+	v.push_back("aa");
+	v.push_back("aa");
+	v.push_back("ab");
+	cout << solution(3, 2, v) << endl;//4
+
+	v.clear();
+	v.push_back("cc");
+	v.push_back("aa");
+	v.push_back("aa");
+	v.push_back("cc");
+	cout << solution(4, 2, v) << endl;//8
+
+	v.clear();
+	v.push_back("ff");
+	v.push_back("aa");
+	v.push_back("cc");
+	v.push_back("aa");
+	v.push_back("aa");
+	v.push_back("cc");
+	v.push_back("dd");
+	v.push_back("ff");
+	cout << solution(8, 2, v) << endl;//8
+
+	v.clear();
+	v.push_back("hgnhu");
+	v.push_back("crshv");
+	v.push_back("ukhvl");
+	v.push_back("mjhqb");
+	v.push_back("gshot");
+	v.push_back("mqmjj");
+	v.push_back("agjkk");
+	v.push_back("qulkk");
 	cout << solution(8, 5, v) << endl;//8
+
+	v.clear();
+	v.push_back("kknkk");
+	v.push_back("kkskk");
+	v.push_back("kkhvl");
+	v.push_back("kkmjj");
+	v.push_back("kkjkk");
+	v.push_back("kklkk");
+	cout << solution(6, 5, v) << endl;//8
 	
 	return 0;
 }
